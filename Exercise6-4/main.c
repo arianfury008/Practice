162f82ae7c024df56ad35a00c552fe42de08c460
ArIaN <aryankh008@gmail.com>
2022-05-13 17:22:21 +0430

    Update main.c

--- Exercise6-4/main.c
+++ Exercise6-4/main.c
@@ -1,10 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #define Hoursbase 7.99
 #define overlay 1.99
 
+struct internet
+{
+    char name[20];
+    int hours;
+    double price;
+};
 
 int main() {
-    printf("motvaje tamrin nmishm !");
-    return 0;
+    struct internet l;
+    printf("Whats your name ? ");
+    scanf("%s",l.name);
+    printf("How many hours do you use the Internet? ");
+    scanf("%d",&l.hours);
+    if(l.hours > 10){
+        l.price = Hoursbase;
+        for(int i = 10;i < l.hours;i++){
+            l.price = l.price + overlay;
+        }
+    } else{
+        l.price = Hoursbase;
+    }
+
+    printf("tmam hazine hay masrafi shoma %lf hast",l.price);
+
+    FILE *log; // The C library function FILE
+
+    log=fopen("internet.txt","w");// create and open file // "w" mode : Creates an empty file for writing. If a file with the same name already exists, its content is erased and the file is considered as a new empty file.
+    if (log == NULL)
+    {
+        fputs("Error at opening File!", stderr); // if log not been created so return null we should send error then close program
+        exit(1);
+    }
+
+    fwrite(&l,sizeof(l),1,log); // Write to file
+    fclose(log); // close file
+    return  0;
 }
