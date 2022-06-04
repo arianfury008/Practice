d71f6f96c112698df168c42bb27273b312f07e14
ArIaN <aryankh008@gmail.com>
2022-06-04 12:19:02 +0430

    Add files via upload

---
+++ Exercise12-3/bsort-descending.h
@@ -0,0 +1,14 @@
+//
+// Created by aryan on 6/3/2022.
+//
+
+#ifndef EXERCISE12_3_BSORT_DESCENDING_H
+#define EXERCISE12_3_BSORT_DESCENDING_H
+
+void swap(int *xp, int *yp);
+
+void bubbleSortD(int arr[], int n);
+
+void printArray(int arr[], int size);
+
+#endif //EXERCISE12_3_BSORT_DESCENDING_H
