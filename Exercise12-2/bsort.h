097f2660c9a9e87e233bb3c85a47b561657e2e36
ArIaN <aryankh008@gmail.com>
2022-06-04 12:18:27 +0430

    Add files via upload

---
+++ Exercise12-2/bsort.h
@@ -0,0 +1,13 @@
+//
+// Created by aryan on 6/3/2022.
+//
+
+#ifndef EXERCISE12_2_BSORT_H
+#define EXERCISE12_2_BSORT_H
+void swap(int *xp, int *yp);
+
+void bubbleSort(int arr[], int n);
+
+void printArray(int arr[], int size);
+
+#endif //EXERCISE12_2_BSORT_H
